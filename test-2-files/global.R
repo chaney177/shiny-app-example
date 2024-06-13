3e70c51693ae64042b871c7316102287bd9905d4
chaney <hannah.chaney@deltacouncil.ca.gov>
2024-06-13 11:12:17 -0700

    working server

---
+++ test-2-files/global.R
@@ -0,0 +1,9 @@
+# LOAD LIBRARIES ----
+library(shiny)
+library(palmerpenguins)
+library(tidyverse)
+library(shinyWidgets)
+library(markdown)
+
+
+## You can also do you data wrangling here. Any data frame object created in this file will be accessible from the server.
